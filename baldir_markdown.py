9837a3624d4186d7ef9aa1e4bd57be3f6a9e138b
Marc BOUVIER <m.bouvier.dev@gmail.com>
2020-06-14 16:50:24 +0000

    format code

--- baldir_markdown.py
+++ baldir_markdown.py
@@ -4,29 +4,34 @@ import xml.dom.minidom
 current_working_directory = os.getcwd()
 
 markdown_file_name = './markdown-sample.md'
+
+
 def import_code_snippet(source_listing_infos):
     from_line = int(source_listing_infos["from"])
     to_line = int(source_listing_infos["to"])
     file_content = read_source_file(source_listing_infos["source"],)
-    return cut_lines(file_content,from_line,to_line)
+    return cut_lines(file_content, from_line, to_line)
 
-def cut_lines(file_as_string,from_line,to_line):
+
+def cut_lines(file_as_string, from_line, to_line):
     line_array = file_as_string.split('\n')
     line_array = line_array[:(to_line)]
     line_array = line_array[(from_line-1):]
-    
-    
-    result ='\n'.join(line_array)
+
+    result = '\n'.join(line_array)
     return result
 
+
 def format_markdown_snippet(source_listing_infos):
-   md_snippet = "```" + source_listing_infos['lang']+"\n"
-   md_snippet += import_code_snippet(source_listing_infos)
-   md_snippet += '\n```'
-   print(md_snippet)
-   return md_snippet
+    md_snippet = "```" + source_listing_infos['lang']+"\n"
+    md_snippet += import_code_snippet(source_listing_infos)
+    md_snippet += '\n```'
+    print(md_snippet)
+    return md_snippet
 
 # reads file as string
+
+
 def read_source_file(markdown_file_name):
     markdown_sample = open(markdown_file_name)
 
@@ -42,6 +47,8 @@ def read_source_file(markdown_file_name):
 
 # parse xml tag and extract attributes
 #source_listing_start_tag = '<sourceListingStart source="./MyJavaFile.java" from="5" to="5" lang="java"/>'
+
+
 def parse_source_listing_start(xml_tag):
     tag_dom = xml.dom.minidom.parseString(xml_tag)
     tag = tag_dom.childNodes.item(0)
@@ -50,6 +57,7 @@ def parse_source_listing_start(xml_tag):
     for attrName, attrValue in tag.attributes.items():
         source_listing_infos[attrName] = attrValue
     return source_listing_infos
+
 
 md_text = """Markdown preprocessor should replace code snippet between `sourceListingStart` and `sourceListingEnd` with code from the source file.
 
@@ -63,19 +71,24 @@ md_text = """Markdown preprocessor should replace code snippet between `sourceLi
 
 end"""
 
+
 def split_against_source_listing_tags(md_text):
     text_before_start_tag = md_text[:md_text.index('<sourceListingStart')]
     text_from_start_tag = md_text[md_text.index('<sourceListingStart'):]
-    index_end_start_tag = len(text_before_start_tag) + text_from_start_tag.index('/>') +2
+    index_end_start_tag = len(text_before_start_tag) + \
+        text_from_start_tag.index('/>') + 2
     start_tag = md_text[len(text_before_start_tag):index_end_start_tag]
     start_tag_length = len(start_tag)
-    index_start_tag_to_end_tag = text_from_start_tag.index('<sourceListingEnd/>')
-    text_between_start_and_end_tags = text_from_start_tag[start_tag_length:index_start_tag_to_end_tag]
-    index_md_text_until_end_tag = md_text.index('<sourceListingEnd/>') + len('<sourceListingEnd/>')
+    index_start_tag_to_end_tag = text_from_start_tag.index(
+        '<sourceListingEnd/>')
+    text_between_start_and_end_tags = text_from_start_tag[
+        start_tag_length:index_start_tag_to_end_tag]
+    index_md_text_until_end_tag = md_text.index(
+        '<sourceListingEnd/>') + len('<sourceListingEnd/>')
     text_after_end_tag = md_text[index_md_text_until_end_tag:]
     return {
-        "text_before_start_tag":text_before_start_tag,
-        "start_tag":start_tag,
-        "text_between_start_and_end_tags":text_between_start_and_end_tags,
-        "text_after_end_tag":text_after_end_tag
-        }
+        "text_before_start_tag": text_before_start_tag,
+        "start_tag": start_tag,
+        "text_between_start_and_end_tags": text_between_start_and_end_tags,
+        "text_after_end_tag": text_after_end_tag
+    }
--- test_baldir_markdown.py
+++ test_baldir_markdown.py
@@ -1,25 +1,34 @@
 import pytest
-from baldir_markdown import read_source_file , parse_source_listing_start, import_code_snippet,format_markdown_snippet,split_against_source_listing_tags
+from baldir_markdown import read_source_file, parse_source_listing_start, import_code_snippet, format_markdown_snippet, split_against_source_listing_tags
+
 
 def test_read_source_file():
     result = read_source_file('./markdown-sample.md')
     assert result == 'Markdown preprocessor should replace code snippet between `sourceListingStart` and `sourceListingEnd` with code from the source file.\n\n<sourceListingStart source="./MyJavaFile.java" from="5" to="5" lang="java"/>\n\n```java\n        System.out.print("Hello world");\n```\n\n<sourceListingEnd/>\n\nend'
 
+
 def test_parse_source_listing_start():
-    result = parse_source_listing_start('<sourceListingStart source="./MyJavaFile.java" from="5" to="5" lang="java"/>')
-    assert result == {'from': '5', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '5'}
+    result = parse_source_listing_start(
+        '<sourceListingStart source="./MyJavaFile.java" from="5" to="5" lang="java"/>')
+    assert result == {'from': '5', 'lang': 'java',
+                      'source': './MyJavaFile.java', 'to': '5'}
+
 
 def test_import_code_snippet_one_line():
-    code_snippet = import_code_snippet({'from': '5', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '5'})
+    code_snippet = import_code_snippet(
+        {'from': '5', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '5'})
     assert code_snippet == '        System.out.println("Hello world");'
 
 
 def test_import_code_snippet_range():
-    code_snippet = import_code_snippet({'from': '4', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '5'})
+    code_snippet = import_code_snippet(
+        {'from': '4', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '5'})
     assert code_snippet == '\n        System.out.println("Hello world");'
 
+
 def test_import_code_snippet_whole_file():
-    code_snippet = import_code_snippet({'from': '1', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '9'})
+    code_snippet = import_code_snippet(
+        {'from': '1', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '9'})
     assert code_snippet == """public class MyJavaFile {
 
     public static void main(String[] args){
@@ -30,9 +39,11 @@ def test_import_code_snippet_whole_file():
 
 }"""
 
+
 def test_format_markdown_snippet():
-   formatted_snippet = format_markdown_snippet({'from': '1', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '9'})
-   assert formatted_snippet ==  """```java
+    formatted_snippet = format_markdown_snippet(
+        {'from': '1', 'lang': 'java', 'source': './MyJavaFile.java', 'to': '9'})
+    assert formatted_snippet == """```java
 public class MyJavaFile {
 
     public static void main(String[] args){
@@ -43,6 +54,7 @@ public class MyJavaFile {
 
 }
 ```"""
+
 
 def test_split_against_source_listing_tags():
     md_text = """Markdown preprocessor should replace code snippet between `sourceListingStart` and `sourceListingEnd` with code from the source file.
@@ -60,14 +72,14 @@ end"""
     assert splitted_text['text_before_start_tag'] == """Markdown preprocessor should replace code snippet between `sourceListingStart` and `sourceListingEnd` with code from the source file.
 
 """
-    assert splitted_text['start_tag'] =='<sourceListingStart source="./MyJavaFile.java" from="5" to="5" lang="java"/>'
-    assert splitted_text['text_between_start_and_end_tags'] =="""
+    assert splitted_text['start_tag'] == '<sourceListingStart source="./MyJavaFile.java" from="5" to="5" lang="java"/>'
+    assert splitted_text['text_between_start_and_end_tags'] == """
 
 ```java
         System.out.print("Hello world");
 ```
 
 """
-    assert splitted_text['text_after_end_tag'] =="""
+    assert splitted_text['text_after_end_tag'] == """
 
 end"""
